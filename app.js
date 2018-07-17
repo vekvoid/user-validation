4fae88040dde01f8d47cc21cda5828037db0c827
vekvoid <vektrm@gmail.com>
2018-07-17 10:38:23 +0200

    Adding helment to secure express

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const multer = require('multer')();
 const CONFIG = require('./config/config');
 const user = require('./routes/v1/user.route');
@@ -6,6 +7,7 @@ const user = require('./routes/v1/user.route');
 const app = express();
 const router = express.Router();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(multer.none());
